3c442cd35abe1215dd28cd6368cffa6748b6c2bb
Jonas Vautherin <jonas.vautherin@gmail.com>
2019-07-31 23:29:53 +0200

    check availability of protoc-gen-grpc-web in generator.sh and print an error message if not in the PATH

--- generator.sh
+++ generator.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+
+set -e
+
 WORK_DIR="./src"
 PROTO_DIR="proto/protos"
 SDK_DIR="${WORK_DIR}/dronecode_sdk"
 JS_IMPORT_STYLE="commonjs"
 PROTOS=`find ${PROTO_DIR} -name "*.proto" -type f`
 
+command -v protoc-gen-grpc-web >/dev/null 2>&1 || { echo "ERROR: 'protoc-gen-grpc-web' is required (find it here: https://github.com/grpc/grpc-web/releases)!"; exit 1; }
+
 function generateForNode {
-    echo "[+] Working on: ${PROTOS}"
+    echo "  [+] Working on: ${PROTOS}"
 
     grpc_tools_node_protoc \
       --js_out=import_style=commonjs,binary:$SDK_DIR \
@@ -19,7 +24,7 @@ function generateForNode {
 function generateForWeb {
   for PROTO_FILE in $PROTOS; do
     MODULE_NAME=`echo $(basename -- ${PROTO_FILE}) | cut -f 1 -d '.'`
-    echo "[+] Working on: ${MODULE_NAME}"
+    echo "  [+] Working on: ${MODULE_NAME}"
 
     protoc \
       -I$PROTO_DIR \
@@ -29,7 +34,7 @@ function generateForWeb {
   done
 }
 
-echo "[+] Generating plugins from "
+echo "[+] Generating plugins for grpc-web "
 generateForWeb
 echo "[+] Done"
 
