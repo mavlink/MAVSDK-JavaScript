cd9d6ec35369538516ddf1ed3c76b218da254be5
Brian Phillips <unipheas@protonmail.com>
2020-05-02 20:25:53 -0700

    updated action.js to connect all functions for the frontend

--- src/mavsdk/action/action.js
+++ src/mavsdk/action/action.js
@@ -1,4 +1,36 @@
-const { ArmRequest, DisarmRequest, TakeoffRequest, LandRequest } = require('./action_pb');
+const {
+  ActionResult,
+  ArmRequest,
+  ArmResponse,
+  DisarmRequest,
+  DisarmResponse,
+  GetMaximumSpeedRequest,
+  GetMaximumSpeedResponse,
+  GetReturnToLaunchAltitudeRequest,
+  GetReturnToLaunchAltitudeResponse,
+  GetTakeoffAltitudeRequest,
+  GetTakeoffAltitudeResponse,
+  KillRequest,
+  KillResponse,
+  LandRequest,
+  LandResponse,
+  RebootRequest,
+  RebootResponse,
+  ReturnToLaunchRequest,
+  ReturnToLaunchResponse,
+  SetMaximumSpeedRequest,
+  SetMaximumSpeedResponse,
+  SetReturnToLaunchAltitudeRequest,
+  SetReturnToLaunchAltitudeResponse,
+  SetTakeoffAltitudeRequest,
+  SetTakeoffAltitudeResponse,
+  TakeoffRequest,
+  TakeoffResponse,
+  TransitionToFixedWingRequest,
+  TransitionToFixedWingResponse,
+  TransitionToMulticopterRequest,
+  TransitionToMulticopterResponse
+   } = require('./action_pb');
 const { ActionServicePromiseClient } = require('./action_grpc_web_pb');
 
 class Action {
@@ -22,15 +54,71 @@ class Action {
       return this.plugin.disarm(request);
     }
 
-    takeoff() {
-        const request = new TakeoffRequest();
-        return this.plugin.takeoff(request);
+    getMaximumSpeed() {
+      const request = new GetMaximumSpeedRequest();
+      return this.plugin.getMaximumSpeed(request);
+    }
+
+    getReturnToLaunchAltitude() {
+      const request = new GetReturnToLaunchAltitudeRequest();
+      return this.plugin.getReturnToLaunchAltitude(request);
+    }
+
+    getTakeoffAltitude() {
+      const request = new GetTakeoffAltitudeRequest();
+      return this.plugin.getTakeoffAltitude(request);
+    }
+
+    kill() {
+      const request = new KillRequest();
+      return this.plugin.kill(request);
     }
 
     land() {
       const request = new LandRequest();
       return this.plugin.land(request);
     }
+
+    reboot() {
+      const request = new RebootRequest();
+      return this.plugin.reboot(request);
+    }
+
+    returnToLaunch() {
+      const request = new ReturnToLaunchRequest();
+      return this.plugin.returnToLaunch(request);
+    }
+
+    setMaximumSpeed() {
+      const request = new SetMaximumSpeedRequest();
+      return this.plugin.setMaximumSpeed(request);
+    }
+
+    setReturnToLaunchAltitude() {
+      const request = new SetReturnToLaunchAltitudeRequest();
+      return this.plugin.setReturnToLaunchAltitude(request);
+    }
+
+    setTakeoffAltitude() {
+      const request = new SetTakeoffAltitudeRequest();
+      return this.plugin.setTakeoffAltitude(request);
+    }
+
+    takeoff() {
+        const request = new TakeoffRequest();
+        return this.plugin.takeoff(request);
+    }
+
+    transitionToFixedWing() {
+      const request = new TransitionToFixedWingRequest();
+      return this.plugin.transitionToFixedWing(request);
+    }
+
+    transitionToMulticopter() {
+      const request = new TransitionToMulticopterRequest();
+      return this.plugin.transitionToMulticopter(request);
+    }
+
 }
 
 
